d5d38db3af83aac4e6f2b1234c054f6dc8168eb5
Aliaksandr Kassirau <aliaksandr.kassirau@gmail.com>
2016-02-15 18:22:35 +0300

    304 Not Modified

--- image-service/image-uploader/src/main/java/com/conference/image/uploader/ImageService.java
+++ image-service/image-uploader/src/main/java/com/conference/image/uploader/ImageService.java
@@ -1,10 +1,14 @@
 package com.conference.image.uploader;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
@@ -15,19 +19,21 @@ import org.springframework.stereotype.Component;
 public class ImageService {
 
     private static final String FILE_PATH = "d:\\FIFA.png";
-
+    
     @GET
     @Path("/get")
     @Produces("image/png")
-    public Response getFile() {
-
+    public Response getFile(@Context Request request) {
+        Calendar calendar = Calendar.getInstance();
         File file = new File(FILE_PATH);
-
-        ResponseBuilder response = Response.ok((Object) file);
-        response.header("Content-Disposition",
-            "attachment; filename=image_from_server.png");
-        return response.build();
-
+        calendar.setTimeInMillis(file.lastModified());
+        Date lastModified = calendar.getTime();
+        ResponseBuilder responseBuilder = request
+                .evaluatePreconditions(lastModified);
+        if (responseBuilder == null) {
+            responseBuilder = Response.ok(file).lastModified(lastModified);
+        }
+        return responseBuilder.build();
     }
-    
+
 }
